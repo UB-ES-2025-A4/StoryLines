eea32ddfbdefdc219bf2f07c5cae635383e33e63
Aleix15 <aleix.maquinen.jr@gmail.com>
2025-12-01 16:48:03 +0100

    Fix error with tests

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,5 +1,13 @@
 export default {
   testEnvironment: "node",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   transform: {},
+
+  // Muy importante → cargar el mock ANTES de cualquier test
+  setupFiles: ["<rootDir>/jest.setup.js"],
+
+  // Permitir ES Modules
+  extensionsToTreatAsEsm: [".js"],
+
+  // Evitar que Jest intente transformar cosas de node_modules
+  moduleFileExtensions: ["js", "json", "node"],
 };
