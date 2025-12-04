e7fbc63f0d8613be07d659b0a7365911ec792111
Aleix15 <aleix.maquinen.jr@gmail.com>
2025-12-04 21:24:02 +0100

    Remove unnecessary comment from Jest configuration

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,11 +1,8 @@
 export default {
   testEnvironment: "node",
   transform: {},
-
-  // Muy importante para Jest + ESM: evita errores de import/export
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1.js",
   },
-
   setupFiles: ["<rootDir>/jest.setup.mjs"],
 };
