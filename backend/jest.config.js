53ff8818b94fdcbc6f4b9a0bd563fdc67eb17280
Aleix15 <aleix.maquinen.jr@gmail.com>
2025-12-04 21:17:24 +0100

    Refactor Jest configuration and update test script for improved module handling

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,5 +1,14 @@
 export default {
   testEnvironment: "node",
   transform: {},
+
+  // NECESARIO PARA EVITAR EL BUG CON ES MODULES
+  extensionsToTreatAsEsm: [".js"],
+
+  // NECESARIO PARA QUE JEST NO ENTRE EN RECURSIÓN TRANSFORMANDO ARCHIVOS
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1.js",
+  },
+
   setupFiles: ["<rootDir>/jest.setup.mjs"],
 };
